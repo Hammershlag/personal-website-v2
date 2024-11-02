7caf84206ebc201f1c0c3d4f68f8bf01b5bb3d75
tomek <tomekzbroszczyk@gmail.com>
2024-11-02 13:26:19 +0100

    technologies logos

--- src/data/TechnologiesList.jsx
+++ src/data/TechnologiesList.jsx
@@ -5,10 +5,8 @@ const technologiesList = [];
 function TechnologiesList() {
     if (technologiesList.length === 0) {
         technologiesData.forEach(tech => {
-            console.log(tech.logo_title);
             try {
                 tech.logo = require(`../static/technologies/${tech.logo_title}`);
-                console.log(tech.logo);
             } catch (error) {
                 console.error(`Logo for ${tech.title} not found`);
             }
